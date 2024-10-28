4c479802a8ae44ed54206695bf8531df38ed2c1b
kkirrin <khlebnikov-2016@mail.ru>
2024-10-28 23:41:30 +1000

    feat: подключил шрифты

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <header className="fixed container">
+            <div>
+                <span>Footer</span>
+            </div>
+            
+
+            <div>
+                Nav
+            </div>
+        </header>
+    )
+}
+
+
+export default Footer;
---
+++ components/Header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header className="fixed container">
+            <div className="flex justify-between items-center">
+                <div>
+                    <span>Logo</span>
+                </div>
+                
+
+                <div>
+                    Menu
+                </div>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header;
