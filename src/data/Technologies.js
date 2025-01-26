5fc8e23cdbf0544880ebde7b8305cfa7ab718b91
kkirrin <khlebnikov-2016@mail.ru>
2025-01-26 21:28:43 +1000

    feat: правит пути к картинкам

--- src/data/Technologies.js
+++ src/data/Technologies.js
@@ -1,28 +1,28 @@
 export const tech_list = [
     {
         'name': 'HTML',
-        'image': 'https://kkirrin.github.io/kkirrin_web_site/public/imgs/html.svg',
+        'image': 'https://kkirrin.github.io/kkirrin_web_site/imgs/html.svg',
         'alt': 'html'
     },
 
     {
         'name': 'CSS',
-        'image': 'https://kkirrin.github.io/kkirrin_web_site/public/imgs/css.svg'
+        'image': 'https://kkirrin.github.io/kkirrin_web_site/imgs/css.svg'
     },
 
     {
         'name': 'JS',
-        'image': 'https://kkirrin.github.io/kkirrin_web_site/public/imgs/js.svg'
+        'image': 'https://kkirrin.github.io/kkirrin_web_site/imgs/js.svg'
     },
 
     {
         'name': 'React',
-        'image': 'https://kkirrin.github.io/kkirrin_web_site/public/imgs/reactjs.svg'
+        'image': 'https://kkirrin.github.io/kkirrin_web_site/imgs/reactjs.svg'
     },
     
     {
         'name': 'Node',
-        'image': 'https://kkirrin.github.io/kkirrin_web_site/public/imgs/nodejs.svg'
+        'image': 'https://kkirrin.github.io/kkirrin_web_site/imgs/nodejs.svg'
     },
 ]
     
