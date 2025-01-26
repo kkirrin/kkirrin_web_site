37e60ed7f5155b3dec066426e1e78992f0c6ea97
kkirrin <khlebnikov-2016@mail.ru>
2025-01-26 22:01:47 +1000

    feat: закончил править пути

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,73 +1,73 @@
 export const Projects = [
     {
         'title': 'Avtopotencial',
-        'image': '/imgs/auto1.png',
+        'image': '/kkirrin-web-site/imgs/auto1.png',
         'link': 'https://avtopotencial-dv.ru'
     },
 
     {
         'title': 'DMC K-Pop SHOP',
-        'image': '/imgs/dmc.png',
+        'image': '/kkirrin-web-site/imgs/dmc.png',
         'link': 'https://dmckpop.shop/'
     },
 
     {
         'title': 'autoproussr',
-        'image': '/imgs/auto2.png',
+        'image': '/kkirrin-web-site/imgs/auto2.png',
         'link': 'https://autoproussur.ru'
     },
 
     {
         'title': 'INSIDE360',
-        'image': '/imgs/inside360.png',
+        'image': '/kkirrin-web-site/imgs/inside360.png',
         'link': 'https://www.inside360.ru/'
     },
 
      {
         'title': 'СЗ Инвест Сити',
-        'image': '/imgs/city.png',
+        'image': '/kkirrin-web-site/imgs/city.png',
         'link': 'https://cz-invest-city.ru/'
     },
 
     {
         'title': 'Формат Вкуса',
-        'image': '/imgs/forma.png',
+        'image': '/kkirrin-web-site/imgs/forma.png',
         'link': 'https://format-vkusa.ru'
     },
 
     {
         'title': 'Tovarka Shop',
-        'image': '/imgs/tovarka.png',
+        'image': '/kkirrin-web-site/imgs/tovarka.png',
         'link': 'https://tovarka-shop.ru'
     },
 
     {
         'title': 'Vlad Decor',
-        'image': '/imgs/vlad.png',
+        'image': '/kkirrin-web-site/vlad.png',
         'link': 'https://vlad-dekor.ru'
     },
 
     {
         'title': 'Gruzomir',
-        'image': '/imgs/gruz.png',
+        'image': '/kkirrin-web-site/imgs/gruz.png',
         'link': 'https://gruzomir.ru'
     },
 
     // {
     //     'title': 'RealTransVostok',
-    //     'image': '/imgs/real.png',
+    //     'image': '/kkirrin-web-site/imgs/real.png',
     //     'link': 'https://realtransvostok.ru'
     // },
 
     {
         'title': 'Roliz Moto',
-        'image': '/imgs/roliz.png',
+        'image': '/kkirrin-web-site/imgs/roliz.png',
         'link': 'https://roliz-moto.ru'
     },
 
     // {
     //     'title': 'Alians',
-    //     'image': '/imgs/eco.png',
+    //     'image': '/kkirrin-web-site/imgs/eco.png',
     //     'link': 'https://eva.ru'
     // }
 ]
